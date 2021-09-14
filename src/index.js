cc9c8a90fc1c27a9ba112e1e3fd780a26d87c286
trishan <trishansaha43@gmail.com>
2021-09-14 13:21:46 +0600

    kjasdfk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Clock ({ local }) {
-  return (
-    <h1 className='heading'>
-      <span className='text'>{new Date().toLocaleTimeString(local)}</span>
-    </h1>
-  )
+class Clock extends React.Component {
+  render () {
+    return (
+      <h1 className='heading'>
+        <span className='text'>
+          {new Date().toLocaleTimeString(this.props.local)}
+        </span>
+      </h1>
+    )
+  }
 }
 
 ReactDOM.render(<Clock local='bn-BD' />, document.getElementById('root'))
