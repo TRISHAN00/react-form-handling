8fe276f1be341f2d15c2cec8e6841b3698156f61
trishan <trishansaha43@gmail.com>
2021-09-15 13:08:18 +0600

    clearInterval

--- src/component/Clock.js
+++ src/component/Clock.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
 class Clock extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { date: new Date() }
-  }
+  state = { date: new Date() }
 
   componentDidMount () {
-    setInterval(() => {
-      this.setState({
-        date: new Date()
-      })
+    this.clearInterval = setInterval(() => {
+      this.tick()
     }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.clearInterval)
+  }
+
+  tick () {
+    this.setState({
+      date: new Date()
+    })
   }
 
   render () {
