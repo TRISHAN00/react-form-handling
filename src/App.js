48f867888402f0937d561567919348c0d8bacf73
trishan <trishansaha43@gmail.com>
2021-10-03 00:32:29 +0600

    starting functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { Component } from 'react'
-import ClickComponent from './components/ClickComponent'
-import ThemeContext from './components/contexts/themeContext'
-import Counter from './components/Counter'
-import Section from './components/Section'
+import React from 'react'
 
-export default class App extends Component {
-  state = { theme: 'dark' }
-  render () {
-    const { theme } = this.state
-    return (
-      <div>
-        <Counter>
-          {(count, incremenetCount) => (
-            <ClickComponent count={count} incremenetCount={incremenetCount} />
-          )}
-        </Counter>
-        <ThemeContext.Provider value={{ theme: theme }}>
-          <Section />
-        </ThemeContext.Provider>
-      </div>
-    )
-  }
+function App () {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
 }
+
+export default App
