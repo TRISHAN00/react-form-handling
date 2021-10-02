e73ba306756a7971a31ec4c64061c7bc49e1b751
trishan <trishansaha43@gmail.com>
2021-10-02 11:08:27 +0600

    click counter handle start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import React, { Component } from 'react'
-import ThemeContext from './components/contexts/themeContext'
-import Counter from './components/Counter'
-import HoverCounter from './components/HoverCounter'
-import Section from './components/Section'
+import { Component } from 'react'
 
 export default class App extends Component {
-  state = { theme: 'dark' }
-
   render () {
-    const { theme } = this.state
-    return (
-      <div>
-        <Counter>
-          {(count, incrementCount) => (
-            <HoverCounter count={count} incrementCount={incrementCount} />
-          )}
-        </Counter>
-        <ThemeContext.Provider value={{ theme: theme }}>
-          <Section />
-        </ThemeContext.Provider>
-      </div>
-    )
+    return <div>hello</div>
   }
 }
--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,17 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function ClickCounter ({ count, incrementCount, theme }) {
-  const style =
-    theme === 'dark' ? { backgroundColor: '#000000', color: '#fff' } : null
-
-  return (
-    <div>
-      <h1 style={style}>count value is : {count}</h1>
-      <button onClick={incrementCount} type='button'>
-        click
-      </button>
-    </div>
-  )
+export default class ClickCounter extends Component {
+  render () {
+    return <div>Click Counter</div>
+  }
 }
-
-export default ClickCounter
