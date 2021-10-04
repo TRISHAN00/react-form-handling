a6eb9652a9de058f9a8359ca409915e0015b8728
trishan <trishansaha43@gmail.com>
2021-10-04 21:23:51 +0600

    make another ref for an example

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useRef } from 'react'
 
 function Form () {
-  const inputRef = useRef()
+  const inputRef = useRef(null)
+  const paraRef = useRef('')
   useEffect(() => {
     // document loaded
     inputRef.current.focus()
+    console.log(paraRef.current)
   }, [])
   return (
     <div>
       <p>
         <input ref={inputRef} type='text' placeholder='enter something' />
+        <p ref={paraRef}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, est.
+        </p>
       </p>
     </div>
   )
