80904f829e1c3e931d5f7699f8993531377cf741
trishan <trishansaha43@gmail.com>
2021-10-06 13:16:53 +0600

    useReducer

--- src/components/lib/reducer.js
+++ src/components/lib/reducer.js
@@ -2,8 +2,6 @@ const array = [1, 2, 3, 4]
 
 const reducer = (prevValue, currentValue) => prevValue + currentValue
 
-
-
-const output = array.reduce(reducer)
+const output = array.reduce(reducer, 5)
 
 console.log(output)
